0473176d5c2e1954ceff284d9ea6f2db0a87cd0a
Luis Albaran <luisalbaran@pop-os.localdomain>
2021-03-22 16:23:31 -0500

    improve ui login and register

--- frontend-app/gquestions-app/tailwind.config.js
+++ frontend-app/gquestions-app/tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        danger: '#ff5f40',
+        info: {
+          900: '#234e52',
+          800: '#285e61',
+        }
+      },
+      fontFamily: {
+        manrope: ['Manrope', ]
+      }
+    },
   },
   variants: {
     extend: {},
